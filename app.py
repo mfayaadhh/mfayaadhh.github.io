e3caa42703b79e532244276589ae6248851b2189
mfayaadhh <131738020+mfayaadhh@users.noreply.github.com>
2023-04-25 11:53:05 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+st.set_page_config(page_title= "Website", page_icon= ":skull:", layout= 'wide')
+
+
+#----Header----
+st.subheader("This is subheader :nerd_face:")
+st.title("This is title :disguised_face:")
+st.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
